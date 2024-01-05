c68c5bc7c820bf92a90de8644361e2d61d089797
paulius-sak <aspauliusas@gmail.com>
2024-01-05 10:22:40 +0200

    task 3 done

--- 2024-01-03- JS papildoma/script.js
+++ 2024-01-03- JS papildoma/script.js
@@ -219,3 +219,212 @@ console.log(slisedArr2301)
 
 let newArr2 = [slisedArr230, slisedArr2301]
 console.log(newArr2.flat())
+
+console.log('-----------------------------------------------------------------------------')
+// 3. Tęsiant darbą su ankstesniu masyvu atlikti šias užduotis naudojant filter metodą. Kiekvienai užduočiai sukurti naują kintamąjį, jame išsaugoti reikšmę, kurią grąžina filter metodas, ir šį kintamąjį išvesti į konsolę:
+// 3.1. Gauti tik teigiamus skaičius.
+// 3.2. Gauti tik neigiamus skaičius.
+// 3.3. Gauti tik skaičius, kurie dalinasi iš 2.
+// 3.4. Gauti tik skaičius, kurie dalinasi iš 3.
+// 3.5. Gauti tik skaičius, kurie dalinasi iš 5.
+// 3.6. Gauti tik skaičius, kurie dalinasi iš 11.
+// 3.7. Gauti tik skaičius, kurie dalinasi iš 31.
+// 3.8. Gauti tik skaičius, kurie dalinasi iš 2 ir iš 3.
+// 3.9. Gauti tik skaičius, kurie dalinasi iš 3 ir iš 7.
+// 3.10. Gauti tik skaičius, kurie dalinasi iš 5 ir iš 9.
+// 3.11. Gauti tik skaičius, kurie dalinasi iš 5 ir iš 11.
+// 3.12. Gauti tik skaičius, kurie dalinasi iš 2, 8 ir 12.
+// 3.13. Gauti tik skaičius, kurie dalinasi iš 2 arba iš 3.
+// 3.14. Gauti tik skaičius, kurie dalinasi iš 3 arba iš 5.
+// 3.15. Gauti tik skaičius, kurie dalinasi iš 5 arba iš 6.
+// 3.16. Gauti tik skaičius, kurie dalinasi iš 7 arba iš 8.
+// 3.17. Gauti tik skaičius, kurie dalinasi iš 9 arba iš 13.
+// 3.18. Gauti tik skaičius, kurie dalinasi iš 2, 3 arba iš 5.
+// 3.19. Gauti tik skaičius, kurie dalinasi iš 5, 7 arba iš 9.
+// 3.20. Gauti tik skaičius, kurie dalinasi iš 7, 8 arba iš 11.
+// 3.21. Gauti tik skaičius, kurie dalinasi iš 9, 12 arba iš 13.
+// 3.22. Gauti tik skaičius, kurie yra didesni už 100.
+// 3.23. Gauti tik skaičius, kurie yra didesni už 555.
+// 3.24. Gauti tik skaičius, kurie yra didesni arba lygūs 888.
+// 3.25. Gauti tik skaičius, kurie yra didesni arba lygūs 6789.
+// 3.26. Gauti tik skaičius, kurie yra mažesni už 50.
+// 3.27. Gauti tik skaičius, kurie yra mažesni už 1000.
+// 3.28. Gauti tik skaičius, kurie yra mažesni arba lygūs -1.
+// 3.29. Gauti tik skaičius, kurie yra mažesni arba lygūs -5564.
+// 3.30. Gauti tik skaičius, kurie yra mažesni už 1000 ir didesni už 500.
+// 3.31. Gauti tik skaičius, kurie yra mažesni už 100 ir didesni už 0.
+// 3.32. Gauti tik skaičius, kurie yra mažesni už 0 ir didesni už -50.
+// 3.33. Gauti tik skaičius, kurie yra mažesni arba lygūs 0 ir didesni už -100.
+// 3.34. Gauti tik skaičius, kurie yra didesni arba lygūs 0 ir mažesni už 55.
+// 3.35. Gauti tik skaičius, kurie yra didesni arba lygūs 444 ir mažesni arba lygūs 654.
+// 3.36. Gauti tik teigiamus skaičius, kurie dalinasi iš 2.
+// 3.37. Gauti tik teigiamus skaičius, kurie dalinasi iš 3.
+// 3.38. Gauti tik neigiamus skaičius, kurie dalinasi iš 4.
+// 3.39. Gauti tik neigiamus skaičius, kurie dalinasi iš 111.
+// 3.40. Gauti tik skaičius, kurie yra didesni už 500 ir kurie dalinasi iš 2.
+// 3.41. Gauti tik skaičius, kurie yra didesni už 1000 ir kurie dalinasi iš 3.
+// 3.42. Gauti tik skaičius, kurie yra mažesni už 1000 ir kurie dalinasi iš 9.
+// 3.43. Gauti tik skaičius, kurie yra mažesni už 500 ir kurie dalinasi iš 2.
+// 3.44. Gauti tik skaičius, kurie yra didesni arba lygūs 33 ir kurie dalinasi iš 3.
+// 3.45. Gauti tik skaičius, kurie yra didesni arba lygūs 444 ir kurie dalinasi iš 12.
+// 3.46. Gauti tik skaičius, kurie yra mažesni arba lygūs 155 ir kurie dalinasi iš 5.
+// 3.47. Gauti tik skaičius, kurie yra mažesni arba lygūs -333 ir kurie dalinasi iš 9.
+// 3.48. Gauti tik skaičius, kurie yra didesni už 100, mažesni už 500 ir kurie dalinasi iš 5.
+// 3.49. Gauti tik skaičius, kurie yra didesni arba lygūs 888, mažesni už 1000 ir kurie dalinasi iš 2.
+// 3.50. Gauti tik skaičius, kurie yra mažesni arba lygūs 888, didesni arba lygūs -333, dalinasi iš 3 ir nėra nulis.
+
+let arr3 = ['start', 2, 3, 5, 11, 155, 888, '15x', 6789, -5564, 'obuolys', -51, 55, 0, 33, 789, 6543, 1, 'trylika', 444, 321, 654, -1, 987, -333, -321, 'end']
+console.log(arr3)
+
+
+let filteredNums31 = arr3.filter(num => num > 0)
+console.log(filteredNums31)
+
+let filteredNums32 = arr3.filter(num => num < 0)
+console.log(filteredNums32)
+
+let filteredNums33 = arr3.filter(num => num % 2 === 0)
+console.log(filteredNums33)
+
+let filteredNums34 = arr3.filter(num => num % 3 === 0)
+console.log(filteredNums34)
+
+let filteredNums35 = arr3.filter(num => num % 5 === 0)
+console.log(filteredNums35)
+
+let filteredNums36 = arr3.filter(num => num % 11 === 0)
+console.log(filteredNums36)
+
+let filteredNums37 = arr3.filter(num => num % 31 === 0)
+console.log(filteredNums37)
+
+let filteredNums38 = arr3.filter(num => num % 3 === 0 && num % 2 === 0)
+console.log(filteredNums38)
+
+let filteredNums39 = arr3.filter(num => num % 3 === 0 && num % 7 === 0)
+console.log(filteredNums39)
+
+let filteredNums310 = arr3.filter(num => num % 5 === 0 && num % 9 === 0)
+console.log(filteredNums310)
+
+let filteredNums311 = arr3.filter(num => num % 5 === 0 && num % 11 === 0)
+console.log(filteredNums311)
+
+let filteredNums312 = arr3.filter(num => num % 2 === 0 && num % 8 === 0 && num % 12 === 0)
+console.log(filteredNums312)
+
+let filteredNums313 = arr3.filter(num => num % 3 === 0 || num % 2 === 0)
+console.log(filteredNums313)
+
+let filteredNums314 = arr3.filter(num => num % 3 === 0 || num % 5 === 0)
+console.log(filteredNums314)
+
+let filteredNums315 = arr3.filter(num => num % 5 === 0 || num % 6 === 0)
+console.log(filteredNums315)
+
+let filteredNums316 = arr3.filter(num => num % 7 === 0 || num % 8 === 0)
+console.log(filteredNums316)
+
+let filteredNums317 = arr3.filter(num => num % 9 === 0 || num % 13 === 0)
+console.log(filteredNums317)
+
+let filteredNums318 = arr3.filter(num => num % 2 === 0 || num % 3 === 0 || num % 5 === 0)
+console.log(filteredNums318)
+
+let filteredNums319 = arr3.filter(num => num % 7 === 0 || num % 9 === 0 || num % 5 === 0)
+console.log(filteredNums319)
+
+let filteredNums320 = arr3.filter(num => num % 7 === 0 || num % 8 === 0 || num % 11 === 0)
+console.log(filteredNums320)
+
+
+let filteredNums321 = arr3.filter(num => num % 9 === 0 || num % 12 === 0 || num % 13 === 0)
+console.log(filteredNums321)
+
+let filteredNums322 = arr3.filter(num => num > 100)
+console.log(filteredNums322)
+
+let filteredNums323 = arr3.filter(num => num > 555)
+console.log(filteredNums323)
+
+let filteredNums324 = arr3.filter(num => num >= 888)
+console.log(filteredNums324)
+
+let filteredNums325 = arr3.filter(num => num >= 6789)
+console.log(filteredNums325)
+
+
+let filteredNums326 = arr3.filter(num => num < 50)
+console.log(filteredNums326)
+
+let filteredNums327 = arr3.filter(num => num < 1000)
+console.log(filteredNums327)
+
+let filteredNums328 = arr3.filter(num => num <= -1)
+console.log(filteredNums328)
+
+let filteredNums329 = arr3.filter(num => num <= -5564)
+console.log(filteredNums329)
+
+let filteredNums330 = arr3.filter(num => num < 1000 && num > 500)
+console.log(filteredNums330)
+
+let filteredNums331 = arr3.filter(num => num < 100 && num > 0)
+console.log(filteredNums331)
+
+let filteredNums332 = arr3.filter(num => num < 0 && num > -50)
+console.log(filteredNums332)
+
+let filteredNums333 = arr3.filter(num => num <= 0 && num > -100)
+console.log(filteredNums333)
+
+let filteredNums334 = arr3.filter(num => num >= 0 && num < 55)
+console.log(filteredNums334)
+
+let filteredNums335 = arr3.filter(num => num >= 444 && num <= 654)
+console.log(filteredNums335)
+
+let filteredNums336 = arr3.filter(num => num > 0 && num % 2 === 0)
+console.log(filteredNums336)
+
+let filteredNums337 = arr3.filter(num => num > 0 && num % 3 === 0)
+console.log(filteredNums337)
+
+let filteredNums338 = arr3.filter(num => num > 0 && num % 4 === 0)
+console.log(filteredNums338)
+
+let filteredNums339 = arr3.filter(num => num > 0 && num % 111 === 0)
+console.log(filteredNums339)
+
+let filteredNums340 = arr3.filter(num => num > 500 && num % 2 === 0)
+console.log(filteredNums340)
+
+let filteredNums341 = arr3.filter(num => num > 1000 && num % 3 === 0)
+console.log(filteredNums341)
+
+let filteredNums342 = arr3.filter(num => num < 1000 && num % 9 === 0)
+console.log(filteredNums342)
+
+let filteredNums343 = arr3.filter(num => num < 500 && num % 2 === 0)
+console.log(filteredNums343)
+
+let filteredNums344 = arr3.filter(num => num >= 33 && num % 3 === 0)
+console.log(filteredNums344)
+
+let filteredNums345 = arr3.filter(num => num >= 444 && num % 12 === 0)
+console.log(filteredNums345)
+
+let filteredNums346 = arr3.filter(num => num <= 155  && num % 5 === 0)
+console.log(filteredNums346)
+
+let filteredNums347 = arr3.filter(num => num <= -333  && num % 9 === 0)
+console.log(filteredNums347)
+
+let filteredNums348 = arr3.filter(num => num > 100  && num < 500  && num % 5 === 0)
+console.log(filteredNums348)
+
+let filteredNums349 = arr3.filter(num => num >= 888  && num < 1000  && num % 2 === 0)
+console.log(filteredNums349)
+
+let filteredNums350 = arr3.filter(num => num <= 888  && num >= -333  && num % 3 === 0 && num !== 0)
+console.log(filteredNums350)
