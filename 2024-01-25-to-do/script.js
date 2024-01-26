756abc0c6e113b5ba17c39eaa865ca51499699c6
paulius-sak <aspauliusas@gmail.com>
2024-01-26 13:14:10 +0200

    more tasks odne

--- 2024-01-25-to-do/script.js
+++ 2024-01-25-to-do/script.js
@@ -2,33 +2,14 @@ const addButton = document.getElementById('add-button')
 const taskInput = document.getElementById('task')
 const taskWrapper = document.getElementById('task-wrapper')
 
-const tasks = []
 
-addButton.addEventListener('click', () => {
-    const taskTitle = taskInput.value.trim(); // Trim to remove leading and trailing whitespaces
-
-    if (taskTitle.length < 3) {
-        // Display an error message or take appropriate action
-        alert('Task title must be at least 3 characters long.');
-        return // Stop execution if the validation fails
-    }
-    
-    const task = {
-        title: taskInput.value,
-        isDone: false,
-        creationDate: new Date,
-    }
+const localStorageTasks = JSON.parse(localStorage.getItem('tasks'))
 
 
+let tasks = localStorageTasks || []
 
-    tasks.push(task)
-    console.log(tasks)
-    console.log(taskTitle)
-
-    taskInput.value = ''
-    taskWrapper.innerHTML = ''
-
-    tasks.reverse().forEach((t, index) => {
+const generateCards = () => {
+    [...tasks].reverse().forEach((t) => {
         const taskCard = document.createElement("div")
         taskCard.setAttribute('class', 'task-card')
 
@@ -37,14 +18,21 @@ addButton.addEventListener('click', () => {
                 return taskObj.title === t.title
             })
             tasks[index].isDone = !tasks[index].isDone
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+
             taskStatus.classList.remove("done", "in-progress")
             taskStatus.classList.add(tasks[index].isDone ? "done" : "in-progress")
 
             taskCard.removeChild(taskStatus)
             taskCard.append(taskStatus)
+
             console.log("Clicked card index:", index)
             console.log("Updated Task:", tasks[index])
             console.log(tasks[index])
+
+            taskWrapper.innerHTML = ''
+
+            generateCards()
         })
 
         const taskTitle = document.createElement('div')
@@ -53,15 +41,14 @@ addButton.addEventListener('click', () => {
         const taskStatus = document.createElement('div')
         taskStatus.classList.add('status-bubble')
         taskStatus.classList.add(t.isDone ? "done" : "in-progress")
+        taskCard.classList.add(t.isDone ? "task-card-done" : "task-card-in-progress")
 
         const taskDate = document.createElement('h6')
         const formattedDate = t.creationDate.toLocaleString('en-GB', {
             year: 'numeric',
             month: 'numeric',
             day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-        });
+        })
     
         taskDate.innerText = formattedDate;
 
@@ -71,5 +58,36 @@ addButton.addEventListener('click', () => {
 
         taskWrapper.append(taskCard)
     })
-})
+}
+
+
+const onAddTask = () => {
+    const taskTitle = taskInput.value.trim() // Trim to remove leading and trailing whitespaces
+
+    if (taskTitle.length < 3) {
+        alert('Task title must be at least 3 characters long.')
+        return
+    }
+    
+    let task = {
+        title: taskInput.value,
+        isDone: false,
+        creationDate: new Date,
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
+    tasks.push(task)
+    console.log(tasks)
+    
+
+    taskInput.value = ''
+    taskWrapper.innerHTML = ''
+
+    generateCards()
+}
+
+addButton.addEventListener('click', onAddTask)
+
+generateCards()
 
