4106272957e5a7dbcdb047bd215e8987cd69400e
paulius-sak <aspauliusas@gmail.com>
2024-02-01 13:07:55 +0200

    new tasks done

---
+++ 2024-02-01- recipes uzduotis POST API/recipeFormScript.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("btn")
+
+const message = document.getElementById("message")
+message.setAttribute("class", "message")
+
+const title = document.getElementById("title")
+const description = document.getElementById("description")
+const instructions = document.getElementById("instructions")
+const ingredients = document.getElementById("ingredients")
+const recipeImg = document.getElementById("recipeImg")
+
+button.addEventListener("click", async () => {
+    const recipeData = {
+        title: title.value,
+        description: description.value,
+        instructions: instructions.value,
+        ingredients: ingredients.value,
+        recipeImg: recipeImg.value,
+    }
+    console.log(recipeData)
+
+
+    const response = await fetch("https://65bb5cf452189914b5bbdaa2.mockapi.io/recipes", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(recipeData),
+    }
+    )
+
+    const addedRecipe = await response.json()
+
+    message.innerHTML = "Recipe was added successfully";
+
+  setTimeout(() => {
+    window.location.assign("./index.html");
+  }, 3000);
+
+
+})
---
+++ 2024-02-01- recipes uzduotis POST API/script.js
@@ -0,0 +1,35 @@
+const wrapper = document.getElementById("wrapper")
+
+const fetchAnimals = async () => {
+    const response = await fetch("https://65bb5cf452189914b5bbdaa2.mockapi.io/recipes")
+
+    const recipes = await response.json()
+    console.log(recipes)
+
+    recipes.forEach((recipe) => {
+        const card = document.createElement("div")
+        card.setAttribute("class", "card")
+        const title = document.createElement("h1")
+        const description = document.createElement("p")
+        const instructions = document.createElement("p")
+        const ingredients = document.createElement("p")
+        const recipeImg = document.createElement("img")
+
+        title.innerText = recipe.title
+        description.innerText = recipe.description
+        instructions.innerText = recipe.instructions
+        ingredients.innerText =`Reikes: ${recipe.ingredients}`
+        recipeImg.src = recipe.recipeImg
+
+
+        card.append(title)
+        card.append(description)
+        card.append(instructions)
+        card.append(ingredients)
+        card.append(recipeImg)
+
+        wrapper.append(card)
+    })
+}
+
+fetchAnimals()
